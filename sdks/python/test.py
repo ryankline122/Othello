a81faf271612be6baef2f5e1afabf611838c2457
Ryan Kline <ryanjkline@protonmail.com>
2023-12-16 09:34:41 -0500

    Adds tests for edge cases

--- sdks/python/test.py
+++ sdks/python/test.py
@@ -28,9 +28,162 @@ class TestGetValidMoves:
       [0, 0, 0, 0, 0, 0, 0, 0]
     ]
     
-    expected = set([(2, 4), (3, 5), (5, 3), (4, 2)])
-    actual = set(map(tuple, client_instance.get_valid_moves()))
+    expected = sorted([[2, 4], [3, 5], [5, 3], [4, 2]])
+    actual = sorted(client_instance.get_valid_moves())
 
+    assert expected == actual
+  
+  def test_get_valid_moves_edge_upper(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 2, 0, 0, 0, 0], 
+      [0, 0, 0, 1, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[0, 3]]
+    actual = sorted(client_instance.get_valid_moves())
+
+    assert expected == actual
+
+  def test_get_valid_moves_edge_upper_left(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 2, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 1, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[0,2]]
+    actual = client_instance.get_valid_moves()
+
+    assert expected == actual
+
+  def test_get_valid_moves_edge_left(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 2, 1, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[0,0]]
+    actual = client_instance.get_valid_moves()
+    
+    assert expected == actual
+
+  def test_get_valid_moves_edge_lower_left(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 1, 0, 0, 0, 0, 0], 
+      [0, 2, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[7,0]]
+    actual = client_instance.get_valid_moves()
+    
+    assert expected == actual
+
+  def test_get_valid_moves_edge_bottom(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 1, 0, 0, 0, 0], 
+      [0, 0, 0, 2, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[7,3]]
+    actual = client_instance.get_valid_moves()
+    
+    assert expected == actual
+
+  def test_get_valid_moves_edge_bottom_right(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [1, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 2, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[7,2]]
+    actual = client_instance.get_valid_moves()
+    
+    assert expected == actual
+
+
+  def test_get_valid_moves_edge_right(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 1, 2, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[0,7]]
+    actual = client_instance.get_valid_moves()
+    
+    assert expected == actual
+    
+  def test_get_valid_moves_edge_upper_right(self, client_instance, board_instance):
+    client_instance.player = 1
+    client_instance.board = board_instance
+    board_instance.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 2, 0, 0], 
+      [0, 0, 0, 0, 1, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    
+    expected = [[0,6]]
+    actual = client_instance.get_valid_moves()
+    
     assert expected == actual
 
   def test_get_valid_moves_endgame1(self, client_instance, board_instance):
@@ -47,8 +200,8 @@ class TestGetValidMoves:
       [2, 2, 2, 2, 2, 2, 2, 2]
     ]
     
-    expected = set([0,3])
-    actual = set(map(tuple, client_instance.get_valid_moves()))
+    expected =[[0,3]]
+    actual = client_instance.get_valid_moves()
 
     assert expected == actual
 
@@ -66,8 +219,8 @@ class TestGetValidMoves:
       [1, 2, 2, 2, 2, 2, 2, 2]
     ]
       
-    expected = set([1,0])
-    actual = set(map(tuple, client_instance.get_valid_moves()))
+    expected = [[1,0]]
+    actual = client_instance.get_valid_moves()
 
     assert expected == actual
 
